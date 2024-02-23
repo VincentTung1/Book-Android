dfd164ecbc2770c69511b69685655b1fe228e61d
董永燊 <471091869@qq.com>
2024-02-23 12:35:43 +0800

    init project..

---
+++ app/src/main/java/com/vincent/book_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vincent.book_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
